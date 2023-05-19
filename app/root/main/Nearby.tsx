7fea4edbd5f55307037123f5ac0ee41ea363e6d4
Miguel Buca <miguelpedrobuca@gmail.com>
2023-05-19 08:01:12 +0100

    chore: add map

--- app/root/main/Nearby.tsx
+++ app/root/main/Nearby.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import MapView, { Callout, Marker } from "react-native-maps";
+import { requestLocationPermission } from "@/src/helper/location";
+import { useBetterState } from "@/src/hooks/useBetterState";
+import { LocationObject } from "expo-location";
 
 const Nearby = () => {
+  const location = useBetterState<LocationObject | null>(null);
+  useEffect(() => {
+    requestLocationPermission((res) => {
+      location.value = res;
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Nearby</Text>
+    <View className="flex-1">
+      {location.value?.coords ? (
+        <MapView
+          region={{
+            latitude: location.value?.coords.latitude,
+            longitude: location.value?.coords.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.015,
+          }}
+          showsBuildings
+          loadingEnabled
+          style={StyleSheet.absoluteFill}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.value?.coords.latitude,
+              longitude: location.value?.coords.longitude,
+            }}
+          >
+            <Callout collapsable>
+              <Text>Minha localização</Text>
+            </Callout>
+          </Marker>
+        </MapView>
+      ) : null}
     </View>
   );
 };
---
+++ src/helper/location.ts
@@ -0,0 +1,40 @@
+import * as Location from "expo-location";
+
+/**
+ * This method request the permissions for approximate and exact device location. It also needs the foreground service permission to subscribe to location updates, while the app is in use.
+ * @param callback
+ */
+export const requestLocationPermission = async (
+  callback?: (
+    location: Location.LocationObject | null,
+    error: string | null
+  ) => void
+) => {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    callback && callback(null, "Permission to access location was denied");
+    return;
+  }
+  let loc: Location.LocationObject;
+  if (/*__DEV__*/ false) {
+    loc = {
+      coords: {
+        accuracy: null,
+        altitude: null,
+        altitudeAccuracy: null,
+        heading: 0,
+        latitude: 41.161865,
+        longitude: -8.604174,
+        speed: 0,
+      },
+      mocked: true,
+      timestamp: Date.now(),
+    };
+  } else {
+    loc = await Location.getCurrentPositionAsync({
+      distanceInterval: 5000,
+    });
+  }
+  callback && callback(loc, null);
+  return loc;
+};
