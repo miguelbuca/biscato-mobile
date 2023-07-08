7be1c58cf4506032e9a50269d635eb39748ab635
miguelbuca <miguelpedrobuca@gmail.com>
2023-07-08 05:55:46 +0100

    chore: some fixes

--- app/root/controller/Find-job.ts
+++ app/root/controller/Find-job.ts
@@ -1,15 +1,22 @@
 import { Api } from "@/src/api";
 import { useBetterState } from "@/src/hooks/useBetterState";
 import { SkillType } from "@/src/interfaces";
+import { isLoading } from "@/src/reduxStore/slices/loader";
 import { useCallback, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 export const useFindJobController = () => {
+  const dispatch = useDispatch();
   const skilltypes = useBetterState<SkillType[]>([]);
 
   const load = useCallback(() => {
-    Api.skillType.all().then(({ data }) => {
-      skilltypes.value = data;
-    });
+    dispatch(isLoading(true));
+    Api.skillType
+      .all()
+      .then(({ data }) => {
+        skilltypes.value = data;
+      })
+      .finally(() => dispatch(isLoading(false)));
   }, []);
 
   useEffect(load, []);
--- app/root/main/controller/Home.ts
+++ app/root/main/controller/Home.ts
@@ -1,24 +1,34 @@
 import { Api } from "@/src/api";
 import { useBetterState } from "@/src/hooks/useBetterState";
 import { Skill, Work } from "@/src/interfaces";
+import { isLoading } from "@/src/reduxStore/slices/loader";
 import { useCallback, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 export const useHomeController = () => {
   const userSkills = useBetterState<Skill[]>([]);
   const works = useBetterState<Work[]>([]);
   const refreshing = useBetterState<boolean>(false);
+  const dispatch = useDispatch();
 
   const load = useCallback(() => {
     try {
+      dispatch(isLoading(true));
       Api.skill?.me().then(({ data }) => {
         userSkills.value = data;
       });
 
-      Api.work?.all().then(({ data }) => {
-        works.value = data;
-      });
+      Api.work
+        ?.all()
+        .then(({ data }) => {
+          works.value = data;
+        })
+        .finally(() => {
+          dispatch(isLoading(false));
+        });
     } catch (error) {
       console.log(error);
+    } finally {
     }
   }, []);
 
