0ee62006ba1eee34d5a0a1ed293c1ca7578f5767
miguelbuca <miguelpedrobuca@gmail.com>
2023-10-09 08:18:45 +0100

    chore(candidateList): design update

--- src/helper/format.ts
+++ src/helper/format.ts
@@ -93,7 +93,22 @@ export const format = () => {
     }
   }
 
+  const numberAsFollow = (value: number) => {
+    if (typeof value !== "number" || isNaN(value)) {
+      throw new Error("Invalid input. Value must be a number.");
+    }
+
+    if (value >= 1000000) {
+      return (value / 1000000).toFixed(1) + "M";
+    } else if (value >= 1000) {
+      return (value / 1000).toFixed(1) + "K";
+    } else {
+      return value.toString();
+    }
+  };
+
   return {
+    numberAsFollow,
     checkDate,
     date,
     colorHex,
